fccef3d0ec810817076f64d4ea72523873bd3fac
Duong Nguyen <dnx04@proton.me>
2024-06-25 09:08:36 +0700

    delete all redundant things

---
+++ themes/heo/components/AnalyticsCard.js
@@ -0,0 +1,32 @@
+import CONFIG from '../config'
+import { siteConfig } from '@/lib/config'
+
+/**
+ * 博客统计卡牌
+ * @param {*} props
+ * @returns
+ */
+export function AnalyticsCard(props) {
+  const targetDate = new Date(siteConfig('HEO_SITE_CREATE_TIME', null, CONFIG))
+  const today = new Date()
+  const diffTime = today.getTime() - targetDate.getTime() // 获取两个日期之间的毫秒数差值
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) // 将毫秒数差值转换为天数差值
+
+  const { postCount } = props
+  return <>
+        <div className='text-md flex flex-col space-y-1 justify-center px-3'>
+            <div className='inline'>
+                <div className='flex justify-between'>
+                    <div>Posts:</div>
+                    <div>{postCount}</div>
+                </div>
+            </div>
+            <div className='inline'>
+                <div className='flex justify-between'>
+                    <div>Days:</div>
+                    <div>{diffDays}</div>
+                </div>
+            </div>
+        </div>
+        </>
+}
